3ccddb390bc793901e3da53f62a2a73696f2831e
Naman Kapasi <namankapasipro@gmail.com>
2020-10-13 21:37:45 -0600

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by hemal kapasi on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
