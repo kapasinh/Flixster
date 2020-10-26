63f57b90456782fa9254b683cf7a517bc6e30918
Naman Kapasi <namankapasipro@gmail.com>
2020-10-25 18:23:10 -0600

    Phase 2 Done

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by hemal kapasi on 10/25/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
